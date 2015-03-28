88eeabe746f4df97ac1d78943952ac03a705b526
Nikolai R Kristiansen <nikolaik@gmail.com>
2015-03-28 18:10:45 +0100

    Allow filtering aliases on destination

--- dpdapi/filters.py
+++ dpdapi/filters.py
@@ -7,6 +7,7 @@ from rest_framework.filters import BaseFilterBackend
 class AliasRegexFilter(django_filters.FilterSet):
     # Ref: https://docs.djangoproject.com/en/1.7/ref/models/querysets/#iregex
     source_regex = django_filters.CharFilter(name="source", lookup_type='iregex')
+    destination_regex = django_filters.CharFilter(name="destination", lookup_type='iregex')
 
     class Meta:
         model = Alias
