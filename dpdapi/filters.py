2a037fbe8ff6111ae73d9cfef5e8a2212a442bd5
Nikolai R Kristiansen <nikolai@lucida.no>
2017-11-01 21:44:15 +0100

    Try fixing regex lookup
    Adds tests

--- dpdapi/filters.py
+++ dpdapi/filters.py
@@ -10,9 +10,14 @@ filters.LOOKUP_TYPES = ['iregex', 'exact', 'iexact']
 class AliasFilter(django_filters.FilterSet):
     # TODO: These do not work
     # Ref: https://docs.djangoproject.com/en/1.7/ref/models/querysets/#iregex
-    source_regex = django_filters.CharFilter(name='source', lookup_expr=['iregex'])
-    destination_regex = django_filters.CharFilter(name='destination', lookup_expr=['iregex'])
+    source_regex = django_filters.CharFilter(name='source')
+    destination_regex = django_filters.CharFilter(name='destination')
 
     class Meta:
         model = Alias
-        fields = ['domain', 'domain__name', 'source_regex', 'destination_regex']
+        fields = {
+            'domain': ['exact'],
+            'domain__name': ['iexact'],
+            'source_regex': ['iregex', 'regex'],
+            'destination_regex': ['iregex', 'regex']
+        }
